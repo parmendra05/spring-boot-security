9aa2364693e4f887a19b0a1bf42520b1938b9121
parmendra kumar <parmendrakumar718@gmail.com>
2023-12-26 23:09:57 +0530

    env

---
+++ src/test/java/com/param/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.param;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
